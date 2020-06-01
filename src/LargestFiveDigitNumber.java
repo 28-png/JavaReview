30320c00b6c928000f4bd5bee2531d762d665f92
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-06-01 10:15:29 -0500

    updated

--- src/LargestFiveDigitNumber.java
+++ src/LargestFiveDigitNumber.java
@@ -1,15 +1,15 @@
 public class LargestFiveDigitNumber {
 
     public static int solve(final String digits) {
-        int dig = Integer.parseInt(digits);
-        for(int i = 0; i <= dig; i++) {
-
+        int digits2 = digits.length();
+        for(int i = 0; i <= digits2; i++) {
+            System.out.println(i);
         }
         return 0; // you code here
     }
 
     public static void main(String[] args) {
-
+    solve("54321");
     }
 
 }
