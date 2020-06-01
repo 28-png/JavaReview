fa84e925326023206688bbca19ba459fe1f8f4b1
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-06-01 08:27:53 -0500

    first commit

---
+++ src/LargestFiveDigitNumber.java
@@ -0,0 +1,12 @@
+public class LargestFiveDigitNumber {
+
+    public static int solve(final String digits) {
+
+        return 0; // you code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
