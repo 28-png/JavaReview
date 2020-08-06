9cde0812515a03157b05c0b38f6aef20de6e9318
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-08-06 08:30:51 -0500

    updated

--- src/LargestFiveDigitNumber.java
+++ src/LargestFiveDigitNumber.java
@@ -1,16 +1,27 @@
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+
 public class LargestFiveDigitNumber {
-    public static int solve(final String digits) {
-        int digits2 = digits.length();
-        for(int i = 0; i <= digits2; i++) {
-            if(digits.charAt(i) > digits.charAt(i)) {
-                return digits.split(digits.charAt(i), 5);
-            }
-        }
-        return 0;
+//    public static int solve(final String digits) {
+//        int digits2 = digits.length();
+//        for(int i = 0; i <= digits2; i++) {
+//            if(digits.charAt(i) > digits.charAt(i)) {
+//                return digits.split(digits.charAt(i), 5);
+//            }
+//        }
+//        return 0;
+//    }
+
+    public Node balancedTree(ArrayList<Integer> inputList) {
+            
     }
 
+
     public static void main(String[] args) {
-    solve("54321");
+//    solve("1234567890");
     }
+
+
 
 }
