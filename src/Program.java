c088f7260d50bf94628a3dc727d92898c85dbba9
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-21 14:04:58 -0600

    started new challenge

---
+++ src/Program.java
@@ -0,0 +1,25 @@
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+
+public class Program {
+    public static int[] arrayOfMultiples(int num, int length) {
+
+            return null;
+
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+}
+
+
+
+}
