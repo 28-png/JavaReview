8ff564c1f08abdb1583aba3f521923537f7ebe2c
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-24 15:02:44 -0600

    updated

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Program {
 //    first challenge in four months
@@ -18,15 +19,19 @@ public static boolean isEqual(int num1, int num2) {
 //third challenge
 //Write a function that accepts an array of 10 integers (between 0 and 9),
 // that returns a string of those numbers in the form of a phone number.
-
+//Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
     public static String createPhoneNumber(int[] numbers) {
-        int[] limit = {10};
-        return "";
+//        int[] firstHalf = Arrays.copyOfRange(numbers, 0, numbers.length/3);
+//                Arrays.toString(firstHalf);
+            String answer = Arrays.toString(numbers);
+
+        return answer.substring(1, answer.length() - 1);
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(arrayOfMultiples(6, 10)));
-        System.out.println(isEqual(1, 1));
+        System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+//        System.out.println(Arrays.toString(arrayOfMultiples(6, 10)));
+//        System.out.println(isEqual(1, 1));
 }
 
 
