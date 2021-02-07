4949807eb48cd34e58fb3177aff7a28621f6efa1
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-02-07 14:54:52 -0600

    updated

--- src/Program.java
+++ src/Program.java
@@ -72,8 +72,8 @@ public static String whoLikesIt(String... names) {
         int count = 0;
         for(int i = 0; i < ar.length; i++)
             for(int j = i + 1; j < ar.length; j++)
-                if ((ar[i] + ar[j]) == n)
-                    count++;
+                if ((ar[i] == ar[j]))
+                    count = n;
         return count;
     }
 
