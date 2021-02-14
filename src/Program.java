8b1342d2ba5f59698957fa8fe5eed2741184f0ac
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-02-14 13:58:45 -0600

    updated

--- src/Program.java
+++ src/Program.java
@@ -73,15 +73,19 @@ public static String whoLikesIt(String... names) {
         int count = 0;
         for(int i = 0; i < n-1; i++)
             for(int j = 0; j < n-i-1; j++)
-                if(ar[i] > ar[j+1]) {
+                if(ar[j] > ar[j+1]) {
                     int temp = ar[j];
                     ar[j] = ar[j+1];
                     ar[j+1] = temp;
-                    System.out.println(ar[j]);
+                    for (int k=0; k<n; ++k)
+                        System.out.print(ar[k] + " ");
+                    System.out.println();
                 }
                     count++;
         return count;
     }
+
+
 
 
 
