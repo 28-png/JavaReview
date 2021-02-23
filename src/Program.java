a523767e13d303df0a287a810e3b191dbf79bc58
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-02-23 08:15:53 -0600

    started new challenge

--- src/Program.java
+++ src/Program.java
@@ -77,6 +77,21 @@ public static List<String> number(List<String> lines) {
            return pairs;
    }
 
+//   Write a simple parser that will parse and run Deadfish.
+//
+//Deadfish has 4 commands, each 1 character long:
+//
+//i increments the value (initially 0)
+//d decrements the value
+//s squares the value
+//o outputs the value into the return array
+//Invalid characters should be ignored.
+//
+//Deadfish.parse("iiisdoso") =- new int[] {8, 64};
+public static int[] parse(String data) {
+    return null;
+}
+
 
     public static void main(String[] args) {
         System.out.println(number(Arrays.asList("a", "b", "c")));
